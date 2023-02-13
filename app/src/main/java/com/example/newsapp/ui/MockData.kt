64c6e497132483aa22e5524cd6717cf6caf1ff89
Junaid <junaidr638@gmail.com>
2023-02-13 17:42:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/ui/MockData.kt
@@ -0,0 +1,71 @@
+package com.example.newsapp.ui
+
+import com.example.newsapp.R
+
+object MockData {
+    val topNewsList = listOf(
+
+        NewsData(
+            1,
+            R.drawable.supreme_court,
+            "N.Srinivisan",
+            "Supreme Court upholds Delimitation in J&K ",
+            "Centre had earlier told court that the delimitation process can't wait in Jammu and Kashmir until 2026. ",
+            "13-02-2023 04:33"
+        ),
+
+        NewsData(
+            2,
+            R.drawable.elon_musk,
+            "Junaid",
+            "Supreme Court upholds Delimitation in J&K ",
+            "Centre had earlier told court that the delimitation process can't wait in Jammu and Kashmir until 2026. ",
+            "13-02-2023 04:33"
+        ),
+
+        NewsData(
+            3,
+            R.drawable.murder_police_us,
+            "Reuters",
+            "Murder in US ",
+            "Centre had earlier told court that the delimitation process can't wait in Jammu and Kashmir until 2026. ",
+            "13-02-2023 04:33"
+        ),
+
+        NewsData(
+            4,
+            R.drawable.aero_india_us_f35,
+            "ANI",
+            "Lethal F-35 to feature in AeroIndia 2023 ",
+            "Centre had earlier told court that the delimitation process can't wait in Jammu and Kashmir until 2026. ",
+            "13-02-2023 04:33"
+        ),
+
+        NewsData(
+            5,
+            R.drawable.sharad_pawar,
+            "N.Srinivisan",
+            "Sharad Pawar's nephew's jibe at Team Shinde ",
+            "Centre had earlier told court that the delimitation process can't wait in Jammu and Kashmir until 2026. ",
+            "13-02-2023 04:33"
+        ),
+
+        NewsData(
+            6,
+            R.drawable.eggs,
+            "Free Press Kashmir",
+            " Prices of eggs skyrocket in Kashmir ",
+            "Centre had earlier told court that the delimitation process can't wait in Jammu and Kashmir until 2026. ",
+            "13-02-2023 04:33"
+        ),
+
+        NewsData(
+            7,
+            R.drawable.breaking_news,
+            "Anonymous",
+            "Turkey hit by earhtquakes: 7.8 magnitude ",
+            "Centre had earlier told court that the delimitation process can't wait in Jammu and Kashmir until 2026. ",
+            "13-02-2023 04:33"
+        )
+    )
+}
---
+++ app/src/main/java/com/example/newsapp/ui/NewsApp.kt
@@ -0,0 +1,25 @@
+package com.example.newsapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.newsapp.ui.components.DetailScreen
+import com.example.newsapp.ui.components.TopNews
+
+@Composable
+fun NewsApp(){
+
+   Navigation()
+
+}
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "topNews"){
+        composable("topNews") { TopNews(navController) }
+        composable("details") {  DetailScreen(navController)}
+    }
+}
